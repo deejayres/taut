0aa8144cc6a42e46af036a8ea41d316ae84fef3a
deejayres <erinresso@gmail.com>
2016-11-01 13:24:43 -0500

    offload index html to home.html template and create home state

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,9 +1,15 @@
 (function() {
     function config($stateProvider, $locationProvider) {
-        SlocationProvider
+        $locationProvider
             .html5Mode({
                 enabled: true,
                 requireBase:false
+            });
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: '/templates/home.html'
             });
     }
 
