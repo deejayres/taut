cfa525628891b66552c2054ce5d3d123b58cf058
deejayres <erinresso@gmail.com>
2016-11-17 12:46:46 -0600

    separate service and controller functions more clearly

--- dist/scripts/services/Channels.js
+++ dist/scripts/services/Channels.js
@@ -1,29 +1,18 @@
 (function() {
-    function Channels($firebaseArray, Messages) {
+    function Channels($firebaseArray) {
         var Channels = {};
         var ref = firebase.database().ref().child("channels");
-
-        // Firebase query reference as array
         var channels = $firebaseArray(ref);
 
-        // API function to add channel to Firebase
         Channels.addChannel = function(channel) {
             channels.$add(channel);
         };
 
-        // API function to set the actively selected channel and call to Messages API to get associated messages
-        Channels.setActiveChannel = function(channel) {
-            Channels.activeChannel = channel;
-            Messages.getByChannelID(channel.$id);
-            console.log(Channels.activeChannel);
-        };
-
-        // Array of all channels in firebase.
         Channels.all = channels;
         return Channels;
     }
 
     angular
         .module('taut')
-        .factory('Channels', ['$firebaseArray', 'Messages', Channels]);
+        .factory('Channels', ['$firebaseArray', Channels]);
 })();
